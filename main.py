ee213ee57d6c22aae5a93e5563524621999d2849
rohit-gupta99939 <79263762+rohit-gupta99939@users.noreply.github.com>
2022-10-08 22:50:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def predict_banknote(data:NewsClass):
 
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
-#if __name__ == '__main__':
- #   uvicorn.run(app, host='127.0.0.1', port=8000)
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
     
 #uvicorn app:app --reload
